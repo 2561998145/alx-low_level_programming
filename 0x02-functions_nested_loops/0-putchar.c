88e14325575d10a86004f8c48fa677ce238045e2
25619945 <nourhanhadeza1@gmail.com>
2023-02-21 09:45:35 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+/**
+*main - print _putchar ,followd by a new line
+* Return: Always 0 (success) 
+*/  
+  putchar('_');
+  putchar('p');
+  putchar('t');
+  putchar('c');
+  putchar('h');
+  putchar('a');
+  putchar('r');
+  putchar('\n');
+
+  return 0;
+
+  }
