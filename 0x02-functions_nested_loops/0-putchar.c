72d678adf400606183bf11df48874ecbf740d412
2561998145 <122809077+2561998145@users.noreply.github.com>
2023-02-21 20:18:17 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,12 +4,12 @@
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-,int main(void),
+,int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
