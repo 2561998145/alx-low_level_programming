d5221fc43ea6e5471f2cbb334dfef1438866b0f8
2561998145 <122809077+2561998145@users.noreply.github.com>
2023-04-08 16:27:11 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,23 +3,23 @@
 
 /**
  * flip_bits - Calculate the number of bits that need to be flipped to convert one number to another
- * @n: The first number
- * @m: The second number to convert to
+ * @source: The source number
+ * @target: The target number to convert to
  * 
  * Return: The number of bits that need to be flipped
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int source, unsigned long int target)
 {
-    unsigned long int diff = n ^ m; // Calculate the XOR difference between n and m
-    int counter = 0; // Use int for the counter variable
+    unsigned long int diff = source ^ target; 
+    // Calculate the XOR difference between source and target
+    unsigned int counter = 0;
+    // Use unsigned int for the counter variable
 
     // Count the number of set bits in the difference using a loop
-    for (int i = 0; i < sizeof(diff) * 8; i++)
+    for (unsigned int i = 0; diff != 0; i++)
     {
-        if (((diff >> i) & 1) == 1)
-        {
-            counter++;
-        }
+        diff = diff & (diff - 1);
+        counter++;
     }
 
     return counter;
