15789c33a8543764c9cb5a8d9a06a60555b3e501
2561998145 <122809077+2561998145@users.noreply.github.com>
2023-04-08 16:20:20 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,29 +2,26 @@
 #include <stdio.h>
 
 /**
- * flip_bits - Determines the number of bits needed to flip to convert one number to another.
- *
- * @n: First number.
- * @m: Second number to convert to.
- *
- * Return: The number of bits needed to flip.
- */
+
+flip_bits - calculate the number of bits to be flipped to convert one number to another number
+
+@n: the first number
+
+@m: the second number to be converted to
+
+Return: the number of bits that need to be flipped
+*/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	// calculate the bitwise difference between n and m
-	unsigned long int diff = n ^ m;
-	
-	// initialize counter to keep track of the number of flipped bits
-	unsigned int counter = 0;
+unsigned long int diff = n ^ m; // calculate the XOR difference between n and m
+int counter = 0;
 
-	// iterate over the bits in diff and count the number of 1s
-	while (diff != 0)
-	{
-		counter++;
-		// clear the rightmost 1 bit in diff
-		diff &= (diff - 1);
-	}
+// count the number of set bits in the difference using the Brian Kernighan's algorithm
+while (diff != 0)
+{
+counter++;
+diff &= (diff - 1);
+}
 
-	// return the total number of flipped bits
-	return counter;
+return counter;
 }
